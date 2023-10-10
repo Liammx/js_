78b6aaba111303f45b371fc40836066c1c398171
liammx <liammx03@gmial.com>
2023-10-10 14:40:04 +0500

    main js

---
+++ mian.js
@@ -0,0 +1,6 @@
+ let arr=[[{a:{price:20}}],[{a:{price:35}}],[{a:{price:44}}]]
+ console.log(arr[0][0].a.price, arr[1][0].a .price, arr[2][0].a.price)
+ let total = arr[0][0].a.price + arr[1][0].a .price + arr[2][0].a.price
+ console.log(total)
+
+ 
